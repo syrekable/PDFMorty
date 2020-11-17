12e4735c9b3da3d595febf8ab86c5d86fc34b78e
syrekable <jordan2000niedziela@gmail.com>
2020-11-17 19:51:30 +0100

    Dodaj pliki projektów.

---
+++ PDFMorty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PDFMorty.Validation;
+
+namespace PDFMorty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            do
+            {
+                Console.Write("Password: ");
+                password = Console.ReadLine().Replace("Password: ", "");
+            } while (!PasswordValidator.Validate(password));
+            Console.WriteLine("Password correct!");
+        }
+    }
+}
---
+++ PDFMorty/Search/ISearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFMorty.Search
+{
+    public interface ISearch
+    {
+        void AddSearchFilter(IDictionary<string, string?> filters);
+        ushort GetNumberOfPages();
+    }
+}
