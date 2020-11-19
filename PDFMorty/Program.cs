0ea6ea85b74446ec0039149394a31a934e55f537
syrekable <jordan2000niedziela@gmail.com>
2020-11-19 15:46:28 +0100

    REPL works!

--- PDFMorty/Program.cs
+++ PDFMorty/Program.cs
@@ -11,97 +11,107 @@ namespace PDFMorty
     class Program
     {
         private static readonly string SHELL_TEXT = $"{Environment.UserName}@PDFMorty-shell-v.1.0> ";
+        private static bool EXIT = false;
         static void Main(string[] args)
         {
             string password, answer;
             Searchable category;
             //check password
-            
             do
             {
-                Console.Write($"{SHELL_TEXT}Password: ");
+                Console.Write("Password: ");
                 password = Console.ReadLine().Replace("Password: ", string.Empty);
             } while (!Validation.Validation.ValidatePassword(password));
-            
+
             Console.WriteLine("Password correct!");
-            
-            //get search category
-            do
+            GreetTheUser();
+
+            //TODO: make it less ugly pls
+            //main program loop
+            while (!EXIT)
             {
-                Console.WriteLine($"{SHELL_TEXT}What are you looking for?");
-                Console.WriteLine("[c] - character");
-                Console.WriteLine("[l] - location");
-                Console.Write(SHELL_TEXT);
-                answer = Console.ReadLine().Replace(SHELL_TEXT, string.Empty);
-                //MOM I'M A PROGRAMMER :DDD
-                if (answer.ToLower().Equals("c"))
+                do
                 {
-                    category = Searchable.Character;
-                }
-                else if (answer.ToLower().Equals("l"))
-                {
-                    category = Searchable.Location;
-                }
-                else
-                {
-                    category = Searchable.Null;
-                }
-            } while (category.Equals(Searchable.Null));
-
-            //get filters
-            Dictionary<string, string> filters = GetUserDefinedFilters(category);
-
-            Console.WriteLine("Search pending...");
-
-            Search_ search = SearchBuilder.Init()
-                                           .WithSearchCategory(category)
-                                           .WithSearchFilters(filters)
-                                           .Build();
-
-            try
-            {
-                List<dynamic> results = search.GetResult();
-                if (category.Equals(Searchable.Character))
-                {
-                    List<Character> characters = new List<Character>();
-                    foreach (var result in results)
+                    Console.WriteLine($"{SHELL_TEXT}What are you looking for?");
+                    Console.WriteLine("[c] - character");
+                    Console.WriteLine("[l] - location");
+                    Console.Write(SHELL_TEXT);
+                    answer = Console.ReadLine().Replace(SHELL_TEXT, string.Empty);
+                    if (answer.ToLower().Equals("c"))
                     {
-                        Character character = CharacterBuilder.Init()
+                        category = Searchable.Character;
+                    }
+                    else if (answer.ToLower().Equals("l"))
+                    {
+                        category = Searchable.Location;
+                    }
+                    else if (answer.ToLower().Equals("exit"))
+                    {
+                        EXIT = true;
+                        category = Searchable.ABANDON;
+                    }
+                    else
+                    {
+                        category = Searchable.Null;
+                    }
+                } while (category.Equals(Searchable.Null));
+
+                if (EXIT) continue;
+                //get filters
+                Dictionary<string, string> filters = GetUserDefinedFilters(category);
+
+                Console.WriteLine("Search pending...");
+
+                Search_ search = SearchBuilder.Init()
+                                               .WithSearchCategory(category)
+                                               .WithSearchFilters(filters)
+                                               .Build();
+                try
+                {
+                    List<dynamic> results = search.GetResult();
+                    if (category.Equals(Searchable.Character))
+                    {
+                        List<Character> characters = new List<Character>();
+                        foreach (var result in results)
+                        {
+                            Character character = CharacterBuilder.Init()
+                                                                    .WithName(result.name)
+                                                                    .WithStatus(result.status)
+                                                                    .OfGender(result.gender)
+                                                                    .From(result.origin.name)
+                                                                    .SpeciesOf(result.species)
+                                                                    .PlayedIn((ushort)result.episode.Count)
+                                                                    .LastSeenAt(result.location.name)
+                                                                    .Build();
+                            characters.Add(character);
+                        }
+                        _ = new PDFCreator($"characters.pdf", characters);
+                        Console.WriteLine("Enjoy your results, nerd!\n");
+                    }
+                    else
+                    {
+                        List<Location> locations = new List<Location>();
+                        foreach (var result in results)
+                        {
+                            Location location = LocationBuilder.Init()
                                                                 .WithName(result.name)
-                                                                .WithStatus(result.status)
-                                                                .OfGender(result.gender)
-                                                                .From(result.origin.name)
-                                                                .SpeciesOf(result.species)
-                                                                .PlayedIn((ushort)result.episode.Count)
-                                                                .LastSeenAt(result.location.name)
+                                                                .FromDimension(result.dimension)
+                                                                .OfType(result.type)
+                                                                .WithThatManyResidents((ushort)result.residents.Count)
                                                                 .Build();
-                        characters.Add(character);
+                            locations.Add(location);
+                        }
+                        _ = new PDFCreator($"locations.pdf", locations);
                     }
-                    _ = new PDFCreator($"characters.pdf", characters);
                 }
-                else
+                catch (System.Net.Http.HttpRequestException)
                 {
-                    List<Location> locations = new List<Location>();
-                    foreach (var result in results)
-                    {
-                        Location location = LocationBuilder.Init()
-                                                            .WithName(result.name)
-                                                            .FromDimension(result.dimension)
-                                                            .OfType(result.type)
-                                                            .WithThatManyResidents((ushort)result.residents.Count)
-                                                            .Build();
-                        locations.Add(location);
-                    }
-                    _ = new PDFCreator($"locations.pdf", locations);
+                    Console.WriteLine("There were no results for the search with your filters. Try again or exit. \n");
                 }
-            }
-            catch (System.Net.Http.HttpRequestException)
-            {
-                Console.WriteLine("There were no results for the search with your filters.");
             }
             Console.WriteLine($"{SHELL_TEXT}Thanks for using the app!");
         }
-
+        //get search category
         private static Dictionary<string, string> GetUserDefinedFilters(Searchable category)
         {
             Dictionary<string, string> filters = new Dictionary<string, string>();
@@ -125,6 +135,18 @@ namespace PDFMorty
                 filters.Add(filter, value);
             }
             return filters;
+        }
+
+        private static void GreetTheUser()
+        {
+            string logo = @"
+ ____  ____  ____    _  _   __  ____  ____  _  _ 
+(  _ \(    \(  __)  ( \/ ) /  \(  _ \(_  _)( \/ )
+ ) __/ ) D ( ) _)   / \/ \(  O ))   /  )(   )  / 
+(__)  (____/(__)    \_)(_/ \__/(__\_) (__) (__/   
+                            ";
+            Console.WriteLine(logo);
+            Console.WriteLine("Welcome to PDFMorty!\nTo exit, type 'exit' while choosing the search category.\n");
         }
     }
     
--- PDFMorty/Search/Searchable.cs
+++ PDFMorty/Search/Searchable.cs
@@ -5,6 +5,7 @@
         Null = -1,
         Character = 0,
         Location = 1,
-        Episode = 2
+        Episode = 2,
+        ABANDON = -2137
     }
 }
